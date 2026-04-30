277a91eb35be309d3e0335c95b10ff9f3ca50cb8
uxdibyajeet <ux.dibyajeet@gmail.com>
2026-04-30 16:08:24 +0000

    three.js basic setups with scene, camera and orbit controlls

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,34 @@
 // Author: Dibyajeet Kirttania
 // Date: 30 April, 2026
 
-//Silly Stuff
-const author = 'Dibyajeet';
-function sillyFunction () {
-    const appMain = document.querySelector('.app');
-    const helloText = document.createElement('p');
-    appMain.appendChild(helloText);
-    helloText.textContent = `Hello! ${author}`;
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+// Scene, Camera and Renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const app = document.querySelector('.app');
+app.appendChild(renderer.domElement);
+
+//Controls and Loaders
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
+
+//add cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+function animate( time ) {
+    controls.update();
+    renderer.render( scene, camera );
 }
-sillyFunction();
+renderer.setAnimationLoop( animate );
